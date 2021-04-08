c21629e22380176748839f078730d78c6f621399
chr-ibb <46900764+chr-ibb@users.noreply.github.com>
2021-04-07 17:36:22 -0700

    Add exceptions.py

---
+++ sspanel/exceptions.py
@@ -0,0 +1,10 @@
+"""
+sspanel.exceptions
+
+This module contains the set of SSPanel's exceptions
+"""
+
+class SSPanelException(Error):
+	"""There was an ambiguous exception while executing your code
+	"""
+	# TODO
