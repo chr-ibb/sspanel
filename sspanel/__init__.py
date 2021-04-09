7a88a3ee28973cf18ecf80a1adf83a7dfc323a15
chr-ibb <46900764+chr-ibb@users.noreply.github.com>
2021-04-08 20:58:17 -0700

    update __init__.py

--- sspanel/__init__.py
+++ sspanel/__init__.py
@@ -1,5 +1,7 @@
-# Code heading. Description, copyright, license, author, etc. 
+# TODO heading. Description, copyright, license, author, etc. 
 
 # TODO Check compatibility
 
+from .api import start, stop, restart, info
 from .control_panel import ControlPanel
+from .server_info import ServerInfo
