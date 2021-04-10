5406402c7de56b3f639c3732f4fcba0c8b9c864c
chr-ibb <46900764+chr-ibb@users.noreply.github.com>
2021-04-09 18:55:09 -0700

    add utils.py

--- sspanel/utils.py
+++ sspanel/utils.py
@@ -1,6 +1,7 @@
 # TODO header
+# TODO am I using search_between? should I switch them around? 
 
-def get_between(s: str, before: str, after: str):
+def search_between(s: str, before: str, after: str):
 	"""Searches a string and finds the first substring found between the
 	before and after substrings, if they are found.
 	Returns the substring and the index it was found at.
@@ -19,3 +20,22 @@ def get_between(s: str, before: str, after: str):
 	if end < 0: return "", 0
 
 	return s[start:start+end], start + end
+
+
+def get_between(s: str, before: str, after: str):
+	"""Same as search_between, but only returns the resulting string"""
+	return search_between(s, before, after)[0]
+
+
+def multi_find(s: str, f: str):
+	"""finds every occurance of f in s"""
+	res = []
+	scanned = 0
+	while len(s) > 0:
+		found = s.find(f)
+		if found == -1: break
+
+		res.append(found + scanned)
+		s = s[found + 1:]
+		scanned += found + 1
+	return res
