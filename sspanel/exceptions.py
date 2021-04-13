30ee20c21d3ad67898cc31d5b04d54086195ea8e
chr-ibb <46900764+chr-ibb@users.noreply.github.com>
2021-04-13 13:50:31 -0700

    add several exceptions

--- sspanel/exceptions.py
+++ sspanel/exceptions.py
@@ -4,7 +4,41 @@ sspanel.exceptions
 This module contains the set of SSPanel's exceptions
 """
 
-class SSPanelException(Error):
-	"""There was an ambiguous exception while executing your code
-	"""
-	# TODO
+class SSPanelException(Exception):
+	"""There was an ambiguous exception."""
+
+
+class LoginError(SSPanelException):
+	"""Failed to log in."""
+	def __init__(self):
+		super().__init__("Failed to log in. Check credentials (username, password, subuser).")
+
+
+class PanelPasswordError(SSPanelException):
+	"""Failed to retrieve panel password."""
+	def __init__(self):
+		super().__init__("Failed to retrieve panel password.")
+
+
+class ServerStartError(SSPanelException):
+	"""Failed to start server."""
+	def __init__(self):
+		super().__init__("Failed to start server. Check server ID.")
+
+
+class ServerStopError(SSPanelException):
+	"""Failed to stop server."""
+	def __init__(self):
+		super().__init__("Failed to stop server. Check server ID.")
+
+
+class ServerRestartError(SSPanelException):
+	"""Failed to restart server."""
+	def __init__(self):
+		super().__init__("Failed to restart server. Check server ID.")
+
+
+class LimitExceededError(SSPanelException):
+	"""Rate limit exceeded."""
+	def __init__(self, limit):
+		super().__init__(f"There must be {limit} seconds between server actions.")
