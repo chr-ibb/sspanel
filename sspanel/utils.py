3df37487b49eb5aa24a8ae8751457ea5c75a73e1
chr-ibb <46900764+chr-ibb@users.noreply.github.com>
2021-04-12 20:28:04 -0700

    adjust and rename some of the functions

--- sspanel/utils.py
+++ sspanel/utils.py
@@ -1,34 +1,53 @@
 # TODO header
 # TODO am I using search_between? should I switch them around? 
 
-def search_between(s: str, before: str, after: str):
+def find_between(s: str, before: str, after: str):
 	"""Searches a string and finds the first substring found between the
 	before and after substrings, if they are found.
-	Returns the substring and the index it was found at.
+	
+	Returns the indexes of the start and end of the found substring,
+	such that s[start:end] would be the substring. (first character, last character + 1)
+
+	If not found, (0,0) is returned. 
 
 	example:
-	>>> get_between("hayneedlestack", "hay", "stack")
-	('needle', 3)
+	>>> find_between("hayneedlestack", "hay", "stack")
+	(3, 9)
 
-	>>> get_between("hello world", "hay", "stack")
-	("", 0)
+	>>> find_between("hello world", "hay", "stack")
+	(0, 0)
 	"""
 	start = s.find(before) + len(before)
-	if start < 0: return "", 0
+	if start < 0: return 0, 0
 
 	end = s[start:].find(after)
-	if end < 0: return "", 0
+	if end < 0: return 0, 0
 
-	return s[start:start+end], start + end
+	return start, start + end
 
 
-def get_between(s: str, before: str, after: str):
-	"""Same as search_between, but only returns the resulting string"""
-	return search_between(s, before, after)[0]
+def string_between(s: str, before: str, after: str):
+	"""Returns the substring of S that is between the first occurrence of 
+	BEFORE and the next occurrence of AFTER, if it is found.
+
+	example:
+	>>> string_between("hayneedlestack", "hay", "stack")
+	"needle"
+
+	>>> string_between("haystack", "hay", "stack")
+	""
+
+	>>> string_between("hello world", "hay", "stack")
+	None
+
+	"""
+	indexes = find_between(s, before, after)
+	if indexes == (0, 0): return None
+	return s[indexes[0]:indexes[1]]
 
 
 def multi_find(s: str, f: str):
-	"""finds every occurance of f in s"""
+	"""finds every occurrence of substring F in string S"""
 	res = []
 	scanned = 0
 	while len(s) > 0:
