800a5737f7ea2f4db6937511a16658ddd9406e57
chr-ibb <46900764+chr-ibb@users.noreply.github.com>
2021-04-14 03:00:30 -0700

    version 0.1.1

--- sspanel/__version__.py
+++ sspanel/__version__.py
@@ -5,6 +5,6 @@ __description__ = 'Simple API for the SurvivalServers.com Control Panel using HT
 __url__ = 'https://github.com/chr-ibb/sspanel'
 __author_email__ = 'me@chribbotson.com'
 __author__ = 'Christopher Ibbotson'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021 Christopher Ibbotson'
