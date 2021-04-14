af8a8864a0372654414658cdb9740675f35e9db2
chr-ibb <46900764+chr-ibb@users.noreply.github.com>
2021-04-13 20:05:59 -0700

    create __version__.py

---
+++ sspanel/__version__.py
@@ -0,0 +1,10 @@
+# SSPanel
+
+__title__ = 'sspanel'
+__description__ = 'Simple API for the SurvivalServers.com Control Panel using HTTP requests.'
+__url__ = 'https://github.com/chr-ibb/sspanel'
+__author_email__ = 'me@chribbotson.com'
+__author__ = 'Christopher Ibbotson'
+__version__ = '0.1.0'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Christopher Ibbotson'
